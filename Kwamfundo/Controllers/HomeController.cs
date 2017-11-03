7348bdf2bccb0e1e76fe894d0c973c1d5f6a3ae9
siseko Neti <3443307@myuwc.ac.za>
2017-11-03 19:32:15 +0200

    First ASP.NET commit

---
+++ Kwamfundo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kwamfundo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "To submit your query, fill the form below.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.Message = "Thanks!!! Your query has been submitted";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+             return View();
+        }
+    }
+}
---
+++ Kwamfundo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kwamfundo.MvcApplication" Language="C#" %>
